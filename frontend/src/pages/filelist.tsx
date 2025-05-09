7c645dc548423ce1debdbafe5e4e97ad978151a4
mitsuijao <nishima.kun@gmail.com>
2025-05-09 19:27:15 +0900

    docker bit change

--- frontend/src/pages/filelist.tsx
+++ frontend/src/pages/filelist.tsx
@@ -9,12 +9,23 @@ interface FileInfo {
 
 function FileList() {
 	const [files, setFiles] = useState<FileInfo[]>([]);
+	async function handleDelete(filename: string) {
+		const result = await fetch(`http://localhost:8000/delete/${filename}`);
+		if (!result.ok) {
+			console.error("Error delete files");
+			alert("削除に失敗しました");
+		}
+		window.location.reload();
+	}
 
 	useEffect(() => {
 		fetch("http://localhost:8000/getfile")
 			.then((response) => response.json())
 			.then((data: FileInfo[]) => setFiles(data))
-			.catch((error) => console.error("Error fetching files:", error));
+			.catch((error) => {
+				console.error("Error fetching files:", error);
+				alert("取得に失敗しました");
+			});
 	}, []);
 
 	return (
@@ -26,6 +37,7 @@ function FileList() {
 						<th>名前</th>
 						<th>サイズ (バイト)</th>
 						<th>最終更新日時</th>
+						<th>削除</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -34,6 +46,7 @@ function FileList() {
 							<td>{file.name}</td>
 							<td>{file.size}</td>
 							<td>{file.last_modified}</td>
+							<td><button onClick={() => handleDelete(file.name)}>削除</button></td>
 						</tr>
 					))}
 				</tbody>
